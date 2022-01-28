dedab654a55c4dc63aa39074302d430bbe5f2212
disaladamsas <damsasdisala@gmail.com>
2022-01-28 18:06:01 +0530

    Redirect to login page if not authed

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,13 +2,13 @@ exports.authed = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.redirect('/');
+        res.redirect('/auth/login');
     }
 };
 
 exports.notAuthed = (req, res, next) => {
     if (req.isAuthenticated()) {
-        res.redirect('/');
+        res.redirect('/auth/login');
     } else {
         next();
     }
