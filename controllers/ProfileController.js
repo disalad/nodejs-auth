7b81abd7e8313f5d436ee2aee637dc72fed64727
disaladamsas <damsasdisala@gmail.com>
2022-01-30 14:33:54 +0530

    Fix minor bugs

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -4,28 +4,29 @@ const path = require('path');
 exports.update_profile = async (req, res, next) => {
     const uploadFile = (req, res) => {
         return new Promise((resolve, reject) => {
-            if (req.files) {
-                const id = req.user.id.toString();
-                const file = req.files.file;
-                const fileName = file.name;
-                const ext = path.extname(fileName || '').split('.');
-                const ex = ext[ext.length - 1];
-                const pathName = path.join(__dirname + `../../uploads/${id}.${ex}`);
-                file.mv(pathName, err => {
-                    if (err) {
-                        console.log(err.message);
-                        reject(err);
-                    } else {
-                        resolve(`/uploads/${id}.${ex}`);
-                    }
-                });
-            }
+            const id = req.user.id.toString();
+            const file = req.files.file;
+            const fileName = file.name;
+            const ext = path.extname(fileName || '').split('.');
+            const ex = ext[ext.length - 1];
+            const pathName = path.join(__dirname + `../../uploads/${id}.${ex}`);
+            file.mv(pathName, err => {
+                if (err) {
+                    console.log(err.message);
+                    reject(err);
+                } else {
+                    resolve(`/uploads/${id}.${ex}`);
+                }
+            });
         });
     };
 
     try {
         const email = req.user.email;
-        const fileUrl = await uploadFile(req, res);
+        let fileUrl;
+        if (req.files) {
+            fileUrl = await uploadFile(req, res);
+        }
         const updateObj = {
             ...(fileUrl && { imgUrl: fileUrl }),
             ...(req.body.name && { username: req.body.name }),
