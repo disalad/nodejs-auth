01941515efce082c3d6d14a3e8be248a1f64a69a
Disala Damsas <damsasdisala@gmail.com>
2022-02-03 22:43:25 +0530

    Send email with Gmail API instead of nodemailer
    Cause sending emails directly from nodemailer throws security errors by Google

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,46 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 require('dotenv').config();
 
-async function sendEmail(to, verificationUri) {
-    let transporter = nodemailer.createTransport({
-        host: process.env.NODEMAILER_HOST,
-        port: process.env.NODEMAILER_PORT,
-        secure: false,
+const {
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REFRESH_TOKEN,
+    GOOGLE_REDIRECT_URI,
+    NODEMAILER_EMAIL,
+    NODEMAILER_DISPLAYNAME,
+} = process.env;
+
+const oAuth2Client = new google.auth.OAuth2(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URI,
+);
+oAuth2Client.setCredentials({ refresh_token: GOOGLE_REFRESH_TOKEN });
+
+async function sendMail(to, verificationURL) {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
         auth: {
-            user: process.env.NODEMAILER_EMAIL,
-            pass: process.env.NODEMAILER_PASSWORD,
+            type: 'OAuth2',
+            user: NODEMAILER_EMAIL,
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            refreshToken: GOOGLE_REFRESH_TOKEN,
+            accessToken: accessToken,
         },
     });
 
-    let info = await transporter.sendMail({
-        from: `"${process.env.NODEMAILER_DISPLAYNAME}" <${process.env.NODEMAILER_EMAIL}>`,
-        to: to,
-        subject: 'node-passport-auth Email Verification',
-        html: `<p><a href=${verificationUri}>Click here</a> to verify your account</p>`,
-    });
-
-    console.log('Message sent: %s', info.messageId);
+    const mailOptions = {
+        from: `${NODEMAILER_DISPLAYNAME} <${NODEMAILER_EMAIL}>`,
+        to,
+        subject: 'Verification Email',
+        html: `<p><a href=${verificationURL}>Click here</a>to verify your account</p>`,
+    };
+    const result = await transport.sendMail(mailOptions);
+    return result;
 }
 
-module.exports = sendEmail;
+module.exports = sendMail;
