e6dbc52e0b2703c53784119cb40ea702c7448563
Disala Damsas <damsasdisala@gmail.com>
2022-02-04 09:01:08 +0530

    feat: Resend verification email

--- controllers/VerifyController.js
+++ controllers/VerifyController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
+const crypto = require('crypto');
+const sendEmail = require('../utils/sendEmail');
 
-exports.verify_page = (req, res, next) => {
+exports.verify_page = (req, res) => {
     const hideEmail = function (email) {
         return email.replace(/(.{2})(.*)(?=@)/, function (gp1, gp2, gp3) {
             for (let i = 0; i < gp3.length; i++) {
@@ -12,7 +14,7 @@ exports.verify_page = (req, res, next) => {
     res.render('verify', { email: hideEmail(req.user.email) });
 };
 
-exports.verify_user = async (req, res, next) => {
+exports.verify_user = async (req, res) => {
     try {
         const token = req.params.token;
         // Prevent verifing others accounts
@@ -28,6 +30,23 @@ exports.verify_user = async (req, res, next) => {
         req.logout();
         req.flash('success', 'Account is now verified. Please log in again');
         res.redirect('/auth/login');
+        // eslint-disable-next-line no-empty
+    } catch (err) {}
+};
+
+exports.resend_email = async (req, res) => {
+    try {
+        const email = req.user.email;
+        const currentURL = req.protocol + '://' + req.get('host');
+        const user = await User.findOne({ verificationToken: req.user.verificationToken });
+        if (!user) throw new Error('Error');
+        const newToken = crypto.randomBytes(20).toString('hex');
+        user.verificationToken = newToken;
+        await user.save();
+        await sendEmail(email, `${currentURL}/verify/${newToken}`);
+        res.redirect('/verify');
     } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/verify');
     }
 };
--- routes/verify.routes.js
+++ routes/verify.routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const VeifyController = require('../controllers/VerifyController');
+const VerifyController = require('../controllers/VerifyController');
 const { authed, notVerified } = require('../middleware/checkAuth');
 
-router.get('/', authed, notVerified, VeifyController.verify_page);
+router.get('/', authed, notVerified, VerifyController.verify_page);
 
-router.get('/:token', authed, notVerified, VeifyController.verify_user);
+router.get('/resend', authed, notVerified, VerifyController.resend_email);
+
+router.get('/:token', authed, notVerified, VerifyController.verify_user);
 
 module.exports = router;
