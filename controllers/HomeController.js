492a85b980015128d4c6f4b05e5b3c1979f76253
Disala Damsas <damsasdisala@gmail.com>
2022-02-04 19:20:54 +0530

    Improve page query validation in the server

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,8 +1,24 @@
 const User = require('../models/User');
 
-exports.home_page = async (req, res) => {
+exports.home_page = async (req, res, next) => {
+    const roundToNearest5 = x => Math.ceil(x / 5) * 5;
+
     try {
-        const userCount = (req.query.page || 1) * 5;
+        const page = req.query.page;
+        if (page && page.length > 10) {
+            return res.status(414).end();
+        }
+        const userCount = (Number(page) || 1) * 5;
+        const docCount = await User.countDocuments({});
+        if (Number(page) === 0) {
+            return res.redirect('?page=1');
+        } else if (page && isNaN(page)) {
+            console.log('page: ', page);
+            return res.redirect('?page=1');
+        } else if (roundToNearest5(docCount) < userCount) {
+            next();
+            return;
+        }
         const users = await User.find({}).limit(userCount);
         const resultsEnd = userCount <= users.length ? false : true;
         res.render('home', {
